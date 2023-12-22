84792847e31db9076f5fe2fd77f32e17e94155c8
Denis <hopeelove2002@gmail.com>
2023-12-22 11:58:06 +0300

    feat: toggle random character block (my version)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
-import Header from '../header';
-import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
+import React, { Component } from "react";
+import { Col, Row, Container, Button } from "reactstrap";
+import Header from "../header";
+import RandomChar from "../randomChar";
+import ItemList from "../itemList";
+import CharDetails from "../charDetails";
 
-const App = () => {
+export default class App extends Component {
+
+  state = {
+    randomCharActive: false,
+    randomCharContent: <RandomChar />
+  }
+
+  toggleRandomCharActive = () => {
+    this.setState(({randomCharActive}) => ({
+      randomCharActive: !randomCharActive
+    }));
+
+    if (this.state.randomCharActive) {
+      this.setState({randomCharContent: <RandomChar />});
+    } else {
+      this.setState({randomCharContent: null})
+    }
+  };
+
+  render() {
     return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 6, offset: 3}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
+      <>
+        <Container>
+          <Header />
+        </Container>
+        <Container>
+          <Row>
+            <Col lg={{ size: 6, offset: 3 }} className='d-flex flex-column'>
+              {this.state.randomCharContent}
+              
+              <Button outline color="info" onClick={this.toggleRandomCharActive}>
+                Toggle enable random character block
+              </Button>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col md="6">
+              <ItemList />
+            </Col>
+            <Col md="6">
+              <CharDetails />
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
-};
-
-export default App;
+  }
+}
