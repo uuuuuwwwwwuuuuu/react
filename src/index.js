6a8bdf9ed05aca4f0e936c6d2cab136de5617a70
Denis <hopeelove2002@gmail.com>
2023-12-25 21:01:45 +0300

    info: more about redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 
-const reducer = (state = 0, action) => {    //непосредственные действия (action)
-  switch (action.type) {
-    case 'INC':
-      return state + 1
-    default: 
-      return state;
-  }
-}
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case "INC":
+            return state + 1;
+        case "DEC":
+            return state - 1;
+        case "RND":
+            return state + action.value;
+        default:
+            return state;
+    }
+};
 
-const store = createStore(reducer);     //создаёт стор
-store.subscribe(() => {                 //выполняет эту функцию после каждого изменения
-  console.log(store.getState());
+const store = createStore(reducer);
+const inc = () => ({ type: "INC" });
+const dec = () => ({ type: "DEC" });
+const rnd = (value) => ({ type: "RND", value });
+
+const update = () => {
+    document.querySelector("#counter").textContent = store.getState();
+};
+
+store.subscribe(update);
+
+document.querySelector("#inc").addEventListener("click", () => {
+    store.dispatch(inc());
 });
 
-store.dispatch({type: 'INC'});          //непосредственное изменение
-store.dispatch({type: 'INC'});
+document.querySelector("#dec").addEventListener("click", () => {
+    store.dispatch(dec());
+});
+
+document.querySelector("#rnd").addEventListener("click", () => {
+    const value = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(value));
+});
