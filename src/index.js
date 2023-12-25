7e50924ff5fd9ab30caa0de78e7112469c9d1753
Denis <hopeelove2002@gmail.com>
2023-12-25 12:59:47 +0300

    info: хуки

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Hook() {
+  const [count, setCount] = useState(0);        //Принимает в себя базовое значение и функцию по изменению этого значения (стэйта)
+  const [data, refreshData] = useState([{name: 'Ivan', sex: ' Male'}])
+
+  return (
+    <div className='m-4'>
+      <h1>Вы кликнули: {count} раз</h1>
+      <button className='p-2' onClick={() => setCount(count + 1)}>+1</button> {/* Добавит и отрисует + 1 с коунтеру */}
+      {data.map(item => {
+        return (
+          <h1>Name: {item.name}, sex: {item.sex}</h1>
+        )
+      })}
+      <button onClick={() => refreshData(data => [...data, {name: 'Alex', sex: 'male'}])}>Add data</button> {/* Добавит данные */}
+    </div>
+  )
+}
+
+ReactDOM.render(<Hook /> , document.getElementById('root'));
