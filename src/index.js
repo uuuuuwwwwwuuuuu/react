42b093122472a1d064038f62cc0651c87b052b8d
Denis <hopeelove2002@gmail.com>
2023-12-25 20:04:06 +0300

    info: base about redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const reducer = (state = 0, action) => {    //непосредственные действия (action)
+  switch (action.type) {
+    case 'INC':
+      return state + 1
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(reducer);     //создаёт стор
+store.subscribe(() => {                 //выполняет эту функцию после каждого изменения
+  console.log(store.getState());
+});
+
+store.dispatch({type: 'INC'});          //непосредственное изменение
+store.dispatch({type: 'INC'});
