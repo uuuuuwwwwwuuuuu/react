d28a23c25f394487c4cf3b968c626ce01fde7037
Denis <hopeelove2002@gmail.com>
2023-12-25 13:11:46 +0300

    info: useEffect

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 function Hook() {
-  const [count, setCount] = useState(0);        //Принимает в себя базовое значение и функцию по изменению этого значения (стэйта)
-  const [data, refreshData] = useState([{name: 'Ivan', sex: ' Male'}])
+  const [count, addCount] = useState(0);
+
+  useEffect(() => {       //Позволяет создать функцию, которая будет выполняться после каждой новой отрисовки компонента
+    console.log(Math.random());
+  });
 
   return (
-    <div className='m-4'>
-      <h1>Вы кликнули: {count} раз</h1>
-      <button className='p-2' onClick={() => setCount(count + 1)}>+1</button> {/* Добавит и отрисует + 1 с коунтеру */}
-      {data.map(item => {
-        return (
-          <h1>Name: {item.name}, sex: {item.sex}</h1>
-        )
-      })}
-      <button onClick={() => refreshData(data => [...data, {name: 'Alex', sex: 'male'}])}>Add data</button> {/* Добавит данные */}
-    </div>
+    <>
+      <h1>Вы кликнули {count} раз</h1>
+      <button onClick={() => addCount(count + 1)}>Нажми на меня</button>
+    </>
   )
 }
 
