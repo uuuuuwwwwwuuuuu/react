d48ae7d215b097efdafed6deabb08f405d8b380b
Denis <hopeelove2002@gmail.com>
2023-12-25 21:38:30 +0300

    info: dispatch generator

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
-import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case "INC":
-            return state + 1;
-        case "DEC":
-            return state - 1;
-        case "RND":
-            return state + action.value;
-        default:
-            return state;
-    }
-};
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import {inc, dec, rnd} from './actions';
 
 const store = createStore(reducer);
-const inc = () => ({ type: "INC" });
-const dec = () => ({ type: "DEC" });
-const rnd = (value) => ({ type: "RND", value });
+const {dispatch} = store;
 
 const update = () => {
     document.querySelector("#counter").textContent = store.getState();
 };
-
 store.subscribe(update);
 
-document.querySelector("#inc").addEventListener("click", () => {
-    store.dispatch(inc());
-});
+const incDispatch = bindActionCreators(inc, dispatch);      //создаёт dispatch генератор
+const decDispatch = bindActionCreators(dec, dispatch);
+const rndDispatch = bindActionCreators(rnd, dispatch);
 
-document.querySelector("#dec").addEventListener("click", () => {
-    store.dispatch(dec());
-});
+document.querySelector("#inc").addEventListener("click", incDispatch);
+
+document.querySelector("#dec").addEventListener("click", decDispatch);
 
 document.querySelector("#rnd").addEventListener("click", () => {
     const value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
+    rndDispatch(value);
 });
